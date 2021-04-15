d618cb4051c0d1544b8b2ea7e9ffe81129b598f8
saran <saranbabu777@gmail.com>
2021-04-15 09:53:46 +0530

    Bug fixes
    Signed-off-by: saran <saranbabu777@gmail.com>

--- src/ui/community-portal/projects/community-portal/src/public-api.ts
+++ src/ui/community-portal/projects/community-portal/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of community-portal
  */
 
-export * from './lib/community-portal.service';
 export * from './lib/community-portal.component';
 export * from './lib/community-portal.module';
