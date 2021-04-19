8a71cdd614c230700c019831502cacd6725b941a
Veena Suresh <veenas9876@gmail.com>
2021-04-19 09:18:28 +0400

    adding usrs and login

--- src/ui/discussion-portal/src/app/app-routing.module.ts
+++ src/ui/discussion-portal/src/app/app-routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'discussions',
-  loadChildren: () => import('./discussions/discussions.module')
-  .then(m => m.DiscussionsModule),
-}];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'discussions',
+    loadChildren: () =>
+      import('./discussions/discussions.module').then(
+        (m) => m.DiscussionsModule
+      ),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then((m) => m.UsersModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
